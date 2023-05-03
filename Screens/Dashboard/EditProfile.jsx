35f7f677bc04151b7239906a65f1957f3ed7bdee
Marc Jalkh <marcjalkh.mej@gmail.com>
2023-05-03 17:48:12 +0300

    Profile Pic upload

--- Screens/Dashboard/EditProfile.jsx
+++ Screens/Dashboard/EditProfile.jsx
@@ -1,14 +1,15 @@
     import {View , Text} from 'react-native';
     import { useState,useEffect } from "react";
     import React from "react";
-    import {  Keyboard,  TouchableWithoutFeedback } from 'react-native';
+    import {  Keyboard,  TouchableWithoutFeedback,TouchableOpacity } from 'react-native';
     import { TextInput, Button, Avatar } from "react-native-paper";
 import styles from '../../assets/StyleSheet/Profile';
 import { DatePickerModal } from "react-native-paper-dates";
 import colors from "../../assets/StyleSheet/Colors";
 import { EditProf } from '../../data/LoginSignUp';
 import *  as Location from 'expo-location'; 
-
+import * as ImagePicker from 'expo-image-picker';
+import { uploadImage } from '../../data/LoginSignUp';
 const EditProfile = ({navigation, route}) => {
     const {info,setInfo,userId} = route.params;
     const [Fname,setFname] = useState(info[0]);
@@ -29,14 +30,16 @@ const EditProfile = ({navigation, route}) => {
       [setOpen, setDate]
     );
     function HandlePress(){
-        EditProf(userId,info[4][0],info[4][1],info[4][2],info[4][3],[Fname,Lname,date,info[3],loca]);
-        setInfo([Fname,Lname,date,info[3],info[4],loca]);
-        navigation.navigate('Profile', {info: [Fname,Lname,date,info[3],info[4],loca]} )
+        EditProf(userId,info[4][0],info[4][1],info[4][2],info[4][3],[Fname,Lname,date,info[3],loca,image]);
+        setInfo([Fname,Lname,date,info[3],info[4],loca,image]);
+        navigation.navigate('Profile', {info: [Fname,Lname,date,info[3],info[4],loca,image]} )
     }
 
     const [location,setLocation] = useState(null);
     const [errorMsg,setErrorMsg] = useState(null);
     const [address,setAddress] = useState("");
+    const [image,setImage] = useState(info[6]);
+
     useEffect(() => {
         (async () => {
           
@@ -68,11 +71,39 @@ const EditProfile = ({navigation, route}) => {
       setLoca(text);
     }
 
+    const HandleProfilePic = async () => {
+//Permissions
+      const {granted} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if(!granted){
+        alert('You need to enable permission to access the library');
+        return;
+      }
+
+      //open camera or gallery
+      try{
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
+      if(result.canceled){
+        return;
+      }
+        
+        setImage(await uploadImage(result.assets[0].uri,userId,""));
+        
+    }
+      catch(err){
+      }
+    }
     return(
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         <View style={styles.container}>
             <Text style={styles.Heading}>Edit Profile</Text>
-            <Avatar.Image size={150} style={styles.Avatr} source={require('../../assets/img/logo.png')} />
+            <TouchableOpacity onPress={()=>HandleProfilePic()}>
+            <Avatar.Image size={150} style={styles.Avatr} source={{uri:image}} />
+            </TouchableOpacity>
             <View style={styles.txtContainer}>
             <TextInput
           label="First Name"
--- Screens/Dashboard/Passport.jsx
+++ Screens/Dashboard/Passport.jsx
@@ -1,11 +1,44 @@
 import {View , Text} from 'react-native';
-const Passport = (props) => {
-    return(
-        <View>
-            <Text>Passport</Text>
-        </View>
+import {  Button, Avatar, ActivityIndicator } from "react-native-paper";
+import styles from '../../assets/StyleSheet/Profile';
+import { customFonts } from '../../assets/StyleSheet/Colors';
+import { Image } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-native';
 
-    );
+customFonts();
+const timestamp = (date) => {
+    const dateArr = date.split('-');
+    let year = dateArr[0];
+    const month = dateArr[1];
+    const day = dateArr[2].split('T')[0];
+    //remove first digit of year
+    year = year.slice(1,5);
+    return `${day}/${month}/${year}`;
+}
+
+
+const Passport = ({navigation, route}) => {
+    const {info,setInfo,userId} = route.params;
+    const BD = timestamp(JSON.stringify(info[2]));
+    
+   return(
+    <View>
+            <ScrollView>
+            <View style={styles.container}>
+            <Text style={styles.Heading}>Passport</Text>
+            <Avatar.Image size={150} style={styles.Avatr} source={{uri:info[6]}} />
+            <View style={styles.txtContainer}>
+
+            <Image source={{uri:info[6]}} style={{width:300,height:1000}} />
+            <Text style={styles.txt}>To Change your passport picture click on it and select a new one !</Text>
+            </View>
+            <Button style={styles.btn} mode='outlined' onPress={() => navigation.navigate('EditProfile')}>Save</Button>
+            </View>
+            </ScrollView>
+
+            </View>
+);
 }
 
 export default Passport;
