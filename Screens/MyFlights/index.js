e787e65184eb4b68c8173d19731ffd31f707cbad
Marc Jalkh <marcjalkh.mej@gmail.com>
2023-05-04 20:26:29 +0300

    MyFlights v1

--- Screens/MyFlights/index.js
+++ Screens/MyFlights/index.js
@@ -1,9 +1,25 @@
 import { View, Text, ImageBackground, ScrollView } from "react-native";
 import { Button } from "react-native-paper";
 import styles from "../../assets/StyleSheet/MyFlightsStyles";
+import airports from "../../airports.json";
+import {useEffect, useState} from 'react';
 const OnGoingFlights = (porps) => {
+  const [cntr,setCntr]=useState("")
+  let country="";
+  airports.map((item,index)=>{
+    if(item.code===porps.country.to){
+      country=item.country;
+    }
+  }
+  )
+  useEffect(()=>{
+    setCntr(country);
+
+  }
+  ,[country])
+
   return (
-    <View>
+    <View style={styles.Ongoingcont}>
       <ImageBackground
         source={require("../../assets/img/logo.png")}
         style={styles.Imagecontainer}
@@ -12,13 +28,30 @@ const OnGoingFlights = (porps) => {
           <Text></Text>
         </View>
         <View style={styles.TextContainer}>
-          <Text>Test</Text>
+        <Text>{porps.country.from + " -> "+cntr}</Text>
+        <Text>Plane Code: {porps.country.PlaneCode}</Text>
+        <Text>Date: {porps.country.date}</Text>
+        <Text>Departure Time: {porps.country.time}</Text>
         </View>
       </ImageBackground>
     </View>
   );
 };
 const PastFlights = (porps) => {
+  const [cntr,setCntr]=useState("")
+  let country="";
+  airports.map((item,index)=>{
+    if(item.code===porps.country){
+      country=item.country;
+    }
+  }
+  )
+  useEffect(()=>{
+    setCntr(country);
+
+  }
+  ,[country])
+
   return (
     <View>
       <ImageBackground
@@ -28,8 +61,9 @@ const PastFlights = (porps) => {
         <View style={{ flex: 1 }}>
           <Text></Text>
         </View>
-        <View style={{ height: 20, backgroundColor: "#fffb" }}>
-          <Text>Test</Text>
+        <View style={{ height: 40, backgroundColor: "#fffb" }}>
+        <Text>{cntr}</Text>
+        <Text>{porps.DepDate} </Text>
         </View>
       </ImageBackground>
     </View>
@@ -37,6 +71,7 @@ const PastFlights = (porps) => {
 };
 const MyFlights = ({ navigation, route }) => {
   const { info, UserId } = route.params;
+  console.log(info[7]);
   if (info[7] == []) {
     return (
       <View>
@@ -44,17 +79,29 @@ const MyFlights = ({ navigation, route }) => {
       </View>
     );
   }
+  const firstItem=info[7][0];
   return (
     <View style={{ backgroundColor: "white", flex: 1 }}>
-        <Text>My Flights:</Text>
         <View style={{ marginLeft: "10%" }}>
-          <OnGoingFlights />
+
+        <Text style={styles.HeadingTxt}>My Flights:</Text>
+
+          <OnGoingFlights country ={firstItem}/>
         </View>
-        <Text>More:</Text>
+        <Text style={styles.Moretxt}>More:</Text>
+
         <View style={{ flexDirection:'row',marginBottom:80}}>
         <ScrollView horizontal={true}>
-        <PastFlights />
-            <PastFlights />
+          {
+            info[7].map((item, index) => {
+              return (
+                <View style={styles.smallFlight} key={index}>
+                  <PastFlights country={item.to} DepDate={item.date}/>
+                </View>
+              );
+            }
+            )
+          }
         </ScrollView>
         </View>
       
